1f20eadd7246558d75b100ff38ef758bb43f018a
Tarek Martin <tarek.sperber23@icloud.com>
2026-09-16 11:10:52 +0200

    Update build.ps1 fix wrong Vagrant directory

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ $vagrantMinVersion = "1.9.0"
 $vagrantreloadMinVersion = "0.0.1"
 $packer = "packer.exe"
 $expectedVBoxLocation = "C:\Program Files\Oracle\VirtualBox"
-$expectedVagrantLocation="C:\HashiCorp\Vagrant\bin"
+$expectedVagrantLocation="C:\Program Files\Vagrant\bin"
 
 
 
